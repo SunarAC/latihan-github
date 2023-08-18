d8253927a9eba773ffdfc99a71842df25b092177
SunarAC <sunarac2005@gmail.com>
2023-08-18 13:27:24 +0700

    init

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Login</title>
+</head>
+<body>
+
+<h2>Halaman Login</h2>
+
+<form method="post" action="process_login.php">
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br><br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Login">
+</form>
+
+</body>
+</html>
